44d05b92cdbc2816b91258c2202cab225a7a7199
Bernard Hamann <bernard@yebolocal.com>
2015-09-20 04:13:48 +0200

    Big API change

--- index.js
+++ index.js
@@ -1,102 +1,153 @@
 var axios = require ('axios');
+var _ = require('lodash');
 
 var neData = {
 
-    before: function(pageAPIPath, pathString){
+    meta: function (req, handlerConfig){
+
+        var path = req.path
+        console.log('path');
+        console.log(path);
+
+        var meta = _.find(handlerConfig.config, { path: path });
+
+        console.log('metaeta init');
+        console.log(meta);
+
+        if (meta === undefined){
+
+            var meta = {
+                path: "path not in config",
+                meta:
+                {
+                    title: "Undefined path: " + req.path,
+                    description: "Not found"
+                }
+            };
+
+            return meta
+        }
+
+        if (meta.nedCustom){
+
+            console.log('meta in custom before')
+            console.log(meta)
+
+            var meta = handlerConfig.custom(meta, req)
+
+            console.log('meta in custom after')
+            console.log(meta)
+
+            return meta
+
+        }
+
+        else {
+
+            console.log('meta init else');
+            console.log(meta);
+
+            return meta
+
+            // this.sendMeta(routeMeta);
+
+
+        }
+
+    },
+
+    before: function(meta){
         var self = this;
-        return axios.all([this.getPage(pageAPIPath, pathString)])
-            .then(function(page){
-                var pdNumber = page[0].data[0].pd.pdNumber;
 
-                if (pdNumber === 0 || undefined){
-                    console.log("Requesting < core > for < " + pathString +" >");
+        var nedbNumber = meta.nedBefore.number;
+
+        if (nedbNumber === 1) {
+
+            console.log("Requesting < " + nedbNumber + " nedb packets > for < " + meta.title +" >");
+
+            var nedb1 = meta.nedBefore.nedb1
+            return axios.all([self.getBefore(nedb1)])
+                .then(function(results){
                     return {
-                        page: page[0].data[0]
+                        nedb1: results[0].data
                     }
-                }
+                });
+        }
 
-                else if (pdNumber === 1) {
+        else if (nedbNumber === 2) {
 
-                    console.log("Requesting < core + 1 packet > for < " + pathString +" >");
+            console.log("Requesting < " + nedbNumber + " nedb packets > for < " + meta.title +" >");
 
-                    var pd1 = page[0].data[0].pd.pd1;
-                    return axios.all([self.getData(pd1)])
-                        .then(function(pd){
-                            return {
-                                page: page[0].data[0],
-                                pd1: pd[0].data
-                            }
-                        });
-                }
-
-                else if (pdNumber === 2) {
-
-                    console.log("Requesting < core + 2 packets > for < " + pathString +" >");
-
-                    var pd1 = page[0].data[0].pd.pd1;
-                    var pd2 = page[0].data[0].pd.pd2;
-                    return axios.all([self.getData(pd1),self.getData(pd2)])
-                        .then(function(pd){
-                            return {
-                                page: page[0].data[0],
-                                pd1: pd[0].data,
-                                pd2: pd[1].data
-                            }
-                        });
-                }
-
-                else if (pdNumber === 3) {
-
-                    console.log("Requesting < core + 3 packets > for < " + pathString +" >");
-
-                    var pd1 = page[0].data[0].pd.pd1;
-                    var pd2 = page[0].data[0].pd.pd2;
-                    var pd3 = page[0].data[0].pd.pd3;
-                    return axios.all([self.getData(pd1),self.getData(pd2),self.getData(pd3)])
-                        .then(function(pd){
-                            return {
-                                page: page[0].data[0],
-                                pd1: pd[0].data,
-                                pd2: pd[1].data,
-                                pd3: pd[2].data
-                            }
-                        });
-                }
-
-                else if (pdNumber > 3)  {
-                    console.log("The page had" + pdNumber + "pre render data requests" );
-                    console.log("Only a maximun of 3 pre render data requests are supported");
-                    console.log("If you need more it can be added on request by opening an issue on github");
+            var nedb1 = meta.nedBefore.nedb1
+            var nedb2 = meta.nedBefore.nedb2
+            return axios.all([self.getBefore(nedb1),self.getBefore(nedb2)])
+                .then(function(results){
                     return {
-                        errorMessage: "dataNumber did not match",
-                        dataNumber: dataNumber
+                        nedb1: results[0].data,
+                        nedb2: results[1].data
                     }
-                }
-                else {
-                    console.log("pdNumber Error");
-                    console.log("The page had" + pdNumber + "pre render data requests" );
-                    console.log("Only a maximun of 3 pre render data requests are supported");
-                    console.log("If you need more it can be added on request by opening an issue on github");
+                });
+        }
+
+        else if (nedbNumber === 3) {
+
+            console.log("Requesting < " + nedbNumber + " nedb packets > for < " + meta.title +" >");
+
+            var nedb1 = dataReq.nedBefore.nedb1
+            var nedb2 = dataReq.nedBefore.nedb2
+            var nedb3 = dataReq.nedBefore.nedb3
+            return axios.all([self.getBefore(nedb1),self.getBefore(nedb2),self.getBefore(nedb3)])
+                .then(function(results){
                     return {
-                        errorMessage: "pdNumber error",
-                        pdNumber: pdNumber
+                        nedb1: results[0].data,
+                        nedb2: results[1].data,
+                        nedb3: results[2].data
                     }
-                }
-            })
+                });
+        }
+
+        else if (nedbNumber > 3)  {
+            console.log("The page had" + nedbNumber + "pre render data requests" );
+            console.log("Only a maximun of 3 pre render data requests are supported");
+            console.log("If you need more it can be added on request by opening an issue on github");
+            return {
+                errorMessage: "dataNumber did not match",
+                dataNumber: nedbNumber
+            }
+        }
+        else {
+            console.log("nedbNumber Error");
+            console.log("The page had" + nedbNumber + "pre render data requests" );
+            console.log("Only a maximun of 3 pre render data requests are supported");
+            console.log("If you need more it can be added on request by opening an issue on github");
+            return {
+                errorMessage: "pdNumber error",
+                pdNumber: nedbNumber
+            }
+        }
     },
 
-    getPage: function(pageAPIPath, pathString) {
-        return axios.get(pageAPIPath+pathString);
-    },
-
-    getData: function(pdx) {
-        var path = pdx.path;
-        var query = pdx-query;
+    getBefore: function(nedbx) {
+        var path = nedbx.path;
+        var query = nedbx-query;
         return axios.get(path);
 
     },
+
+    cycleBefore: function(){
+
+    },
+
     after: function(){
         console.log("Future feature for getting data after the page has rendered already and updating the page")
+    },
+
+    cycleAfter: function(){
+
+    },
+
+    query: function(){
+
     }
 
     // pd = pre render data request
--- readme.md
+++ readme.md
@@ -1,3 +1,68 @@
 # ne-data (Node Engine Data)
 
 A tool to request data from api's for pages using the Node Engine methodology
+
+## Get meta for handler
+
+```js
+
+var meta = neData.meta(req, handlerConfig);
+
+```
+
+## Get pre render data from api's 
+
+```js
+
+neData.before(meta)
+    .then(function(data) {
+        renderPage(data);
+    });
+    
+```
+
+## Cycle (Not yet implimented)
+
+Use the query params of the url to get batches of the data and then cycle through the batches 
+
+on the client (update dom)
+
+```js 
+
+neData.cycleAfter(meta)
+    .then(function(data) {
+        updateState(data);
+    });
+
+```
+
+
+on the server (re render)
+
+```js 
+
+neData.cycleAfter(meta)
+    .then(function(data) {
+        renderPage(data);
+    });
+
+```
+
+## Get data to update state after page has rendered (Not yet implimented)
+
+Get data to update state after page has rendered
+
+on the client (update dom)
+
+neData.after(meta)
+    .then(function(data) {
+        updateState(data);
+    });
+    
+on the server (re render)
+
+neData.after(meta)
+    .then(function(data) {
+        renderPage(data);
+    });
+
